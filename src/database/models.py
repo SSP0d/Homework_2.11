e8a48c7e58891509b203a1811d267697baf50cf4
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-17 14:42:39 +0200

    migration

--- src/database/models.py
+++ src/database/models.py
@@ -7,10 +7,10 @@ Base = declarative_base()
 class Contact(Base):
     __tablename__ = "contacts"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(20), index=True, nullable=False, )
-    surname = Column(String(20), index=True, nullable=False)
-    email = Column(String(20), unique=True, index=True, nullable=False)
-    phone = Column(String(10), unique=True, index=True, nullable=False)
+    name = Column(String, index=True, nullable=False, )
+    surname = Column(String, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, unique=True, index=True, nullable=False)
     birthday = Column(DateTime, index=True, nullable=False)
-    description = Column(String(255), index=True, nullable=True)
+    description = Column(String, index=True, nullable=True)
     
